133a0bb65ae77f0ea04141228b2da8511bada888
Leny Luna <leny0023@hotmail.com>
2018-02-10 16:42:51 -0400

    Nitido

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,76 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var bodyParser = require('body-parser');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+var fs = require('fs');
+var path = require('path');
+
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
 });
+
+var buffer = bufferFile('C:/Users/Leny96/Documents/Dc.Universidad/ProyectoFinal/sip.conf');
+var myFile;
+var j = 0;
+
+function formatString(data){
+    var cant = data.match("\n").length;
+    var arr = data;
+    for(var i = 0; i < cant; i++){
+        switch (j){
+            case 0:
+                arr = data.toString().replace("\n",",");
+                j++;
+                break;
+            case 1:
+                data = arr.toString().replace("\n",",");
+                j++;
+                break;
+            case 2:
+                arr = data.toString().replace("\n",",");
+                j--;
+                break;
+        }
+    }
+    data = arr.split(",");
+    return data;
+}
+
+function bufferFile(myPath){
+    return fs.readFile(myPath, 'utf-8', function(err, data){
+        if(err){
+            console.log(err);
+        } else{
+            myFile = data
+            console.log (myFile);
+        }
+    });
+}
+// comment
+router.get('/', function(req, res, next) {
+    var date = new Date();
+    res.render('index',
+        { title: myFile,
+            date: date});
+});
+
+//pruebabsksajhdfsdn
+
+router.post('/form', urlencodedParser, function(req, res) {
+    var data = req.body.trunk;
+    fs.writeFile('C:/Users/Leny96/Documents/Dc.Universidad/ProyectoFinal/sip.conf',data,function(err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('Guardado Satisfactoriamente');
+            buffer = bufferFile('C:/Users/Leny96/Documents/Dc.Universidad/ProyectoFinal/sip.conf');
+            res.redirect('/');
+        }
+    });
+});
+
 
 module.exports = router;
